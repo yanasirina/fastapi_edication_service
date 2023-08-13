65084eabe2ac6cfc3057c4ba11de10ed7ba725e5
Yana Sirina <yanasirina0@yandex.ru>
2023-08-13 20:36:13 +0500

    роуты для удаление, получения и обновления пользователей

--- api/handlers.py
+++ api/handlers.py
@@ -1,10 +1,10 @@
 from typing import Union
 from uuid import UUID
 
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from api.models import UserCreate, ShowUser
+from api.models import UserCreate, ShowUser, DeleteUserResponse, UpdateUserRequest, UpdatedUserResponse
 from db.dals import UserDAL
 from db.session import get_db
 
@@ -78,3 +78,33 @@ async def create_user(body: UserCreate, db: AsyncSession = Depends(get_db)) -> S
     В нашем случае закроет сессию.
     """
     return await _create_new_user(body, db)
+
+
+@user_router.delete("/", response_model=DeleteUserResponse)
+async def delete_user(user_id: UUID, db: AsyncSession = Depends(get_db)) -> DeleteUserResponse:
+    deleted_user_id = await _delete_user(user_id, db)
+    if deleted_user_id is None:
+        raise HTTPException(status_code=404, detail=f"User with id {user_id} not found.")
+    return DeleteUserResponse(deleted_user_id=deleted_user_id)
+
+
+@user_router.get("/", response_model=ShowUser)
+async def get_user_by_id(user_id: UUID, db: AsyncSession = Depends(get_db)) -> ShowUser:
+    user = await _get_user_by_id(user_id, db)
+    if user is None:
+        raise HTTPException(status_code=404, detail=f"User with id {user_id} not found.")
+    return user
+
+
+@user_router.patch("/", response_model=UpdatedUserResponse)
+async def update_user_by_id(
+        user_id: UUID, body: UpdateUserRequest, db: AsyncSession = Depends(get_db)
+) -> UpdatedUserResponse:
+    updated_user_params = body.dict(exclude_none=True)
+    if updated_user_params == {}:
+        raise HTTPException(status_code=422, detail="At least one parameter for user update info should be provided")
+    user = await _get_user_by_id(user_id, db)
+    if user is None:
+        raise HTTPException(status_code=404, detail=f"User with id {user_id} not found.")
+    updated_user_id = await _update_user(updated_user_params=updated_user_params, db=db, user_id=user_id)
+    return UpdatedUserResponse(updated_user_id=updated_user_id)
