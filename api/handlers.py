352f1a48e37fc63d8c0af54e11c3bb301563426b
Yana Sirina <yanasirina0@yandex.ru>
2023-08-13 20:21:32 +0500

    вьюхи для удаления, получения и обновления пользователя

--- api/handlers.py
+++ api/handlers.py
@@ -1,3 +1,6 @@
+from typing import Union
+from uuid import UUID
+
 from fastapi import APIRouter, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
@@ -8,6 +11,7 @@ from db.session import get_db
 user_router = APIRouter()
 
 """Аналог views и url в django"""
+
 
 async def _create_new_user(body: UserCreate, db) -> ShowUser:
     async with db as session:
@@ -25,6 +29,44 @@ async def _create_new_user(body: UserCreate, db) -> ShowUser:
                 email=user.email,
                 is_active=user.is_active,
             )
+
+
+async def _delete_user(user_id, db) -> Union[UUID, None]:
+    async with db as session:
+        async with session.begin():
+            user_dal = UserDAL(session)
+            deleted_user_id = await user_dal.delete_user(
+                user_id=user_id,
+            )
+            return deleted_user_id
+
+
+async def _update_user(updated_user_params: dict, user_id: UUID, db) -> Union[UUID, None]:
+    async with db as session:
+        async with session.begin():
+            user_dal = UserDAL(session)
+            updated_user_id = await user_dal.update_user(
+                user_id=user_id,
+                **updated_user_params
+            )
+            return updated_user_id
+
+
+async def _get_user_by_id(user_id, db) -> Union[ShowUser, None]:
+    async with db as session:
+        async with session.begin():
+            user_dal = UserDAL(session)
+            user = await user_dal.get_user_by_id(
+                user_id=user_id,
+            )
+            if user is not None:
+                return ShowUser(
+                    user_id=user.user_id,
+                    name=user.name,
+                    surname=user.surname,
+                    email=user.email,
+                    is_active=user.is_active,
+                )
 
 
 @user_router.post("/", response_model=ShowUser)
