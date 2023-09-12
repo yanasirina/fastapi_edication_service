8bc6b18ea8d28a6af92e4427c5b05b94c183a30f
Yana Sirina <yanasirina0@yandex.ru>
2023-09-12 23:11:23 +0500

    добавила поле hashed_password в модель

---
+++ migrations/versions/ebbc675886a9_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: ebbc675886a9
+Revises: 96be88d17ea6
+Create Date: 2023-09-12 23:07:44.955407
+
+"""
+import sqlalchemy as sa
+from alembic import op
+
+
+# revision identifiers, used by Alembic.
+revision = "ebbc675886a9"
+down_revision = "96be88d17ea6"
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
