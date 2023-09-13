374ea4deceaa5941f26fa2c5c111224528bcc712
Yana Sirina <yanasirina0@yandex.ru>
2023-09-13 11:50:33 +0500

    функция для генерации jwt токена

--- settings.py
+++ settings.py
@@ -11,3 +11,7 @@ TEST_DATABASE_URL = env.str(
     "REAL_DATABASE_URL",
     default="postgresql+asyncpg://postgres:postgres@0.0.0.0:5435/postgres_test",
 )  # connect string for the database
+
+SECRET_KEY: str = env.str("SECRET_KEY", default="secret_key")
+ALGORITHM: str = env.str("ALGORITHM", default="HS256")
+ACCESS_TOKEN_EXPIRE_MINUTES: int = env.int("ACCESS_TOKEN_EXPIRE_MINUTES", default=30)
