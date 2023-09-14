1ce60ca9cea0686a42937b6f963133c809745d53
Yana Sirina <yanasirina0@yandex.ru>
2023-09-15 00:04:00 +0500

    получение пользователя по токену

--- api/login_handler.py
+++ api/login_handler.py
@@ -5,7 +5,10 @@ from fastapi import APIRouter
 from fastapi import Depends
 from fastapi import HTTPException
 from fastapi import status
+from fastapi.security import OAuth2PasswordBearer
 from fastapi.security import OAuth2PasswordRequestForm
+from jose import jwt
+from jose import JWTError
 from sqlalchemy.ext.asyncio import AsyncSession
 
 import settings
@@ -52,3 +55,32 @@ async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(
         expires_delta=access_token_expires,
     )
     return {"access_token": access_token, "token_type": "bearer"}
+
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/login/token")
+
+
+async def get_current_user_from_token(token: str = Depends(oauth2_scheme), db: AsyncSession = Depends(get_db)):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+    )
+    try:
+        payload = jwt.decode(
+            token=token, key=settings.SECRET_KEY, algorithms=[settings.ALGORITHM]
+        )
+        email: str = payload.get("sub")
+        print("username/email extracted is ", email)
+        if email is None:
+            raise credentials_exception
+    except JWTError:
+        raise credentials_exception
+    user = await _get_user_by_email_for_auth(email=email, db=db)
+    if user is None:
+        raise credentials_exception
+    return user
+
+
+@login_router.get("/test_auth_endpoint")
+async def sample_endpoint_under_jwt(current_user: User = Depends(get_current_user_from_token)):
+    return {"Success": True, "current_user": current_user}
