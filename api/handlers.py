6b85e6fb5052239dd2307abe7166caf5a0a2ea27
Yana Sirina <yanasirina0@yandex.ru>
2023-09-20 21:32:33 +0500

    вынесла вьюхи в отдельный модуль

--- api/handlers.py
+++ api/handlers.py
@@ -1,5 +1,4 @@
 import logging
-from typing import Union
 from uuid import UUID
 
 from fastapi import APIRouter
@@ -8,77 +7,21 @@ from fastapi import HTTPException
 from sqlalchemy.exc import IntegrityError
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from api.actions.user import create_new_user
+from api.actions.user import delete_user
+from api.actions.user import get_user_by_id
+from api.actions.user import update_user
 from api.models import DeleteUserResponse
 from api.models import ShowUser
 from api.models import UpdatedUserResponse
 from api.models import UpdateUserRequest
 from api.models import UserCreate
-from db.dals import UserDAL
 from db.session import get_db
-from hashing import Hasher
 
 user_router = APIRouter()
 logger = logging.getLogger(__name__)
 
-"""Аналог views и url в django"""
-
-
-async def _create_new_user(body: UserCreate, db) -> ShowUser:
-    async with db as session:
-        async with session.begin():
-            user_dal = UserDAL(session)
-            user = await user_dal.create_user(
-                name=body.name,
-                surname=body.surname,
-                email=body.email,
-                hashed_password=Hasher.get_password_hash(body.password),
-            )
-            return ShowUser(
-                user_id=user.user_id,
-                name=user.name,
-                surname=user.surname,
-                email=user.email,
-                is_active=user.is_active,
-            )
-
-
-async def _delete_user(user_id, db) -> Union[UUID, None]:
-    async with db as session:
-        async with session.begin():
-            user_dal = UserDAL(session)
-            deleted_user_id = await user_dal.delete_user(
-                user_id=user_id,
-            )
-            return deleted_user_id
-
-
-async def _update_user(
-    updated_user_params: dict, user_id: UUID, db
-) -> Union[UUID, None]:
-    async with db as session:
-        async with session.begin():
-            user_dal = UserDAL(session)
-            updated_user_id = await user_dal.update_user(
-                user_id=user_id, **updated_user_params
-            )
-            return updated_user_id
-
-
-async def _get_user_by_id(user_id, db) -> Union[ShowUser, None]:
-    async with db as session:
-        async with session.begin():
-            user_dal = UserDAL(session)
-            user = await user_dal.get_user_by_id(
-                user_id=user_id,
-            )
-            if user is not None:
-                return ShowUser(
-                    user_id=user.user_id,
-                    name=user.name,
-                    surname=user.surname,
-                    email=user.email,
-                    is_active=user.is_active,
-                )
+"""urls в django"""
 
 
 @user_router.post("/", response_model=ShowUser)
@@ -90,7 +33,7 @@ async def create_user(body: UserCreate, db: AsyncSession = Depends(get_db)) -> S
     В нашем случае закроет сессию.
     """
     try:
-        return await _create_new_user(body, db)
+        return await create_new_user(body, db)
     except IntegrityError as err:
         logger.error(err)
         raise HTTPException(status_code=503, detail=f"Database error: {err}")
@@ -100,7 +43,7 @@ async def create_user(body: UserCreate, db: AsyncSession = Depends(get_db)) -> S
 async def delete_user(
     user_id: UUID, db: AsyncSession = Depends(get_db)
 ) -> DeleteUserResponse:
-    deleted_user_id = await _delete_user(user_id, db)
+    deleted_user_id = await delete_user(user_id, db)
     if deleted_user_id is None:
         raise HTTPException(
             status_code=404, detail=f"User with id {user_id} not found."
@@ -110,7 +53,7 @@ async def delete_user(
 
 @user_router.get("/", response_model=ShowUser)
 async def get_user_by_id(user_id: UUID, db: AsyncSession = Depends(get_db)) -> ShowUser:
-    user = await _get_user_by_id(user_id, db)
+    user = await get_user_by_id(user_id, db)
     if user is None:
         raise HTTPException(
             status_code=404, detail=f"User with id {user_id} not found."
@@ -129,15 +72,15 @@ async def update_user_by_id(
             detail="At least one parameter for user update info should be provided",
         )
 
-    user = await _get_user_by_id(user_id, db)
+    user = await get_user_by_id(user_id, db)
     if user is None:
         raise HTTPException(
             status_code=404, detail=f"User with id {user_id} not found."
         )
 
     try:
-        updated_user_id = await _update_user(
-            updated_user_params=updated_user_params, db=db, user_id=user_id
+        updated_user_id = await update_user(
+            updated_user_params=updated_user_params, session=db, user_id=user_id
         )
     except IntegrityError as err:
         logger.error(err)
